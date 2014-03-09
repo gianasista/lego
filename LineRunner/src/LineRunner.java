326df8df839ffce3a79c3b100e581855f0459110
gianasista <gianasista@users.sourceforge.net>
2014-03-09 10:13:10 +0100

    minor enhancements and little lejos hack

--- LineRunner/src/LineRunner.java
+++ LineRunner/src/LineRunner.java
@@ -19,9 +19,9 @@ public class LineRunner
 	{
 		System.out.println("Programmstart");
 		//RegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.A);
-		RegulatedMotor motorLeft = Motor.A;
+		RegulatedMotor motorLeft = Motor.B;
 		//RegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.B); 
-		RegulatedMotor motorRight = Motor.B;
+		RegulatedMotor motorRight = Motor.A;
 		motorLeft.resetTachoCount();
 		motorRight.resetTachoCount();
 		motorLeft.rotate(0);
@@ -29,194 +29,57 @@ public class LineRunner
 		motorLeft.setAcceleration(800);
 		motorRight.setAcceleration(800);
 		System.out.println("Motoren erzeugt");
-		 
-		/*
-		System.out.println("L: 100 - R: 100");
-		motorLeft.setSpeed(100);
-		motorRight.setSpeed(100);
-		motorLeft.forward();
-		motorRight.forward();
-		Thread.sleep(2000);
-		
-		System.out.println("L: 200 - R: 200");
-		motorLeft.setSpeed(200);
-		motorRight.setSpeed(200);
-		motorLeft.forward();
-		motorRight.forward();
-		Thread.sleep(2000);
-		
-		System.out.println("L: 300 - R: 300");
-		motorLeft.setSpeed(300);
-		motorRight.setSpeed(300);
-		motorLeft.forward();
-		motorRight.forward();
-		Thread.sleep(2000);
-		
-		System.out.println("L: 300 - R: 100");
-		motorLeft.setSpeed(300);
-		motorRight.setSpeed(100);
-		motorLeft.forward();
-		motorRight.forward();
-		Thread.sleep(2000);
-		
-		System.out.println("L: 100 - R: 300");
-		motorLeft.setSpeed(100);
-		motorRight.setSpeed(300);
-		motorLeft.forward();
-		motorRight.forward();
-		Thread.sleep(2000);
-		
-		System.out.println("L: 0 - R: 200");
-		motorLeft.setSpeed(0);
-		motorRight.setSpeed(200);
-		motorLeft.stop();
-		//motorLeft.forward();
-		motorRight.forward();
-		Thread.sleep(2000);
-		
-		System.out.println("L: 300 - R: 0");
-		motorLeft.setSpeed(300);
-		motorRight.setSpeed(0);
-		motorLeft.stop();
-		//motorLeft.forward();
-		motorRight.forward();
-		Thread.sleep(2000);
-		*/
 		
 		LineDetector lineDetector = new LineDetector(new EV3ColorSensor(SensorPort.S1));
 		lineDetector.setDaemon(true);
 		lineDetector.start();
 		
+		System.out.println("Hack start");
+		motorLeft.setSpeed(100);
+		motorRight.setSpeed(100);
+		lineDetector.getSensorValue();
+		motorLeft.forward();
+		motorRight.forward();
+		lineDetector.getSensorValue();
+		lineDetector.getSensorValue();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("Hack stop");
+	
+		
 		int speedLeft = 0;
 		int speedRight = 0;
 		
-		DetectionMode lastMode = null;
+		int targetPowerLevel = 100;
+		int offset = 31; // (65 - 4)/2 aka Middle White and Black Value
+		int slopeAkaKp = 4; // (0-100)/-31-0)
 		
 		while(!Button.ESCAPE.isDown())
 		{
-			DetectionMode detectedMode = lineDetector.getDetectedMode();
+			int lightValue = lineDetector.getSensorValue();
+			int error = lightValue - offset;
+			int turn = slopeAkaKp * error;
+			speedLeft = targetPowerLevel + turn;
+			speedRight = targetPowerLevel - turn;
 			
-			if (lastMode != detectedMode)
-			{
-				System.out.println("Changed Mode");
-				int speed = 100;
-				switch(detectedMode)
-				{
-				case BLACK: speedLeft =  speed; speedRight =  0;     System.out.println("Black"); break;
-				case EDGE : speedLeft =  speed; speedRight =  speed; System.out.println("Edge");break;
-				case WHITE: speedLeft =  0;     speedRight =  speed; System.out.println("White");break;
-				default:    speedLeft =  10; speedRight =  10; break;
-				}
-				lastMode = detectedMode;
-				System.out.println(String.format("SpeedLeft: %d - SpeedRight: %d", speedLeft, speedRight));
-				
-				motorLeft.setSpeed(speedLeft);
-				motorRight.setSpeed(speedRight);
-				motorLeft.forward();
-				motorRight.forward();
-			}
-			try {
-				Thread.sleep(150);
-				//motorLeft.stop();
-				//motorRight.stop();
-				//System.out.println("Motoren gestoppt... Warten 5 sek");
-				//Thread.sleep(1000);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
-		
-		
-		/*
-		Behavior driveForward = new DriveRobot(motorLeft, motorRight, lineDetector);
-		
-		Behavior[] behaviourList = { driveForward };
-		Arbitrator arbitrator = new Arbitrator(behaviourList);
-		arbitrator.start();
-		*/
-	}
-	
-}
-
-enum DetectionMode { BLACK, EDGE, WHITE	}
-
-class DriveRobot implements Behavior
-{
-	private RegulatedMotor motorLeft;
-	private RegulatedMotor motorRight;
-	private LineDetector lineDetector;
-	private boolean suppressed;
-	
-	//private int INITIAL_SPEED = 100;
-	//private int SPEED_STEP = 25;
-	private int speedLeft;
-	private int speedRight;
-	
-	public DriveRobot(RegulatedMotor motorLeft, RegulatedMotor motorRight, LineDetector lineDetector)
-	{
-		this.motorLeft = motorLeft;
-		this.motorRight = motorRight;
-		this.lineDetector = lineDetector;
-	}
-	
-	@Override
-	public boolean takeControl() 
-	{
-		if(Button.readButtons() == Button.ID_ESCAPE)
-		{
-			suppressed = true;
-			System.exit(1);
-		}
-		return true;
-	}
-
-	@Override
-	public void action() 
-	{
-		System.out.println("\nDriveForward - action");
-		while(!suppressed)
-		{
-			switch(lineDetector.getDetectedMode())
-			{
-				case BLACK: speedLeft =  100; speedRight =  0;   System.out.println("\nB"); break;
-				case EDGE : speedLeft =  100; speedRight =  100; System.out.println("\nE");break;
-				case WHITE: speedLeft =  0;   speedRight =  100; System.out.println("\nW");break;
-				default:    speedLeft =  10;  speedRight =  10; break;
-			}
-			/*
+			System.out.println(String.format("LightValue: %d, Error: %d, Turn: %d, Left: %d, Right: %d", lightValue, error, turn, speedLeft, speedRight));
+			
 			motorLeft.setSpeed(speedLeft);
 			motorRight.setSpeed(speedRight);
-			motorLeft.forward();
+			
 			motorRight.forward();
-			*/
+			motorLeft.forward();
+			
 			try {
-				Thread.sleep(200);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
+				Thread.sleep(10);
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			/*
-			motorLeft.stop();
-			motorRight.stop();
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			*/
-			Thread.yield();
 		}
-		motorLeft.stop();
-		motorRight.stop();
-		//System.exit(1);
-	}
-
-	@Override
-	public void suppress() 
-	{
-		suppressed = true;
 	}
 	
 }
@@ -228,26 +91,10 @@ class LineDetector extends Thread
 	
 	private float lightValue;
 	
-	private static final float BLACK_VALUE = 0.04f;
-	private static final float WHITE_VALUE = 0.61f;
-	
-	private float lowerEdgeThreshold;
-	private float upperEdgeThreshold;
-	
-	//private static final float MIDDLE_VALUE = (BLACK_VALUE-WHITE_VALUE)/2f;
-	
 	public LineDetector(EV3ColorSensor colorSensor)
 	{
 		this.sensorMode = colorSensor.getRedMode();
 		this.sample = new float[sensorMode.sampleSize()];
-		
-		float middle = (WHITE_VALUE - BLACK_VALUE)/2f;
-		float border = (WHITE_VALUE - BLACK_VALUE)/8f; 
-		System.out.println(String.format("Middle: %f - Border: %f - Test: %f", middle, border, 0.02f));
-		this.lowerEdgeThreshold = middle - border;
-		this.upperEdgeThreshold = middle + border;
-		
-		System.out.println(String.format("White: %f - Black: %f - LowerETH: %f - UpperETH: %f", WHITE_VALUE, BLACK_VALUE, lowerEdgeThreshold, upperEdgeThreshold));
 	}
 	
 	public void run()
@@ -259,19 +106,8 @@ class LineDetector extends Thread
         }
     }
 	
-	public boolean isLineLost()
+	public int getSensorValue()
 	{
-		return lightValue > 0.1;
-	}
-	
-	public DetectionMode getDetectedMode()
-	{
-		System.out.println("LightVal: ["+lightValue+"]");
-		if(lightValue < lowerEdgeThreshold)
-			return DetectionMode.BLACK;
-		if(lightValue > upperEdgeThreshold)
-			return DetectionMode.WHITE;
-		
-		return DetectionMode.EDGE;
+		return (int)(lightValue * 100);
 	}
 }
